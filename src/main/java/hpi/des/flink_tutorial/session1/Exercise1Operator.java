c3c923b275c09f9c6b503197d964f7c40218bb0c
pedro <pedro@macboom>
2021-11-19 15:27:16 +0100

    adding solutions, reseting exercises

--- src/main/java/hpi/des/flink_tutorial/session1/Exercise1Operator.java
+++ src/main/java/hpi/des/flink_tutorial/session1/Exercise1Operator.java
@@ -1,24 +1,7 @@
 package hpi.des.flink_tutorial.session1;
 
-import hpi.des.flink_tutorial.util.TaxiRideTuple;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.api.java.tuple.Tuple7;
-
-import java.time.LocalDateTime;
-
 // implement here the class of your operator. Check TransformSourceStreamOperator.java for an example. Don't change the name
 // of the class because it is used in the testing.
-public class Exercise1Operator implements MapFunction<TaxiRideTuple, Tuple7<LocalDateTime, LocalDateTime,
-        Integer, Integer, Integer, Double, Double>> {
-
-    @Override
-    public Tuple7<LocalDateTime, LocalDateTime, Integer, Integer, Integer, Double, Double> map(TaxiRideTuple taxiRideTuple) throws Exception {
-        return new Tuple7<>(
-                taxiRideTuple.f1, taxiRideTuple.f2, taxiRideTuple.f3, taxiRideTuple.f5, taxiRideTuple.f9,
-                taxiRideTuple.f13, taxiRideTuple.f16
-        );
-    }
-
+public class Exercise1Operator{
 }
 
