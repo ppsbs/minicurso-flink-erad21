540b56f7b4ccd1e3c00b0b88eb93df37937ad10b
pedro <pedro@macboom>
2021-11-19 14:57:21 +0100

    adding solutions of session 1

--- src/main/java/hpi/des/flink_tutorial/session1/solution/Exercise1OperatorSolution.java
+++ src/main/java/hpi/des/flink_tutorial/session1/solution/Exercise1OperatorSolution.java
@@ -9,11 +9,9 @@ import java.time.LocalDateTime;
 // implement here the class of your operator. Check TransformSourceStreamOperator.java for an example. Don't change the name
 // of the class because it is used in the testing.
 public class Exercise1OperatorSolution implements MapFunction<TaxiRideTuple, Tuple7<LocalDateTime, LocalDateTime, Integer, Integer, Integer, Double, Double>> {
-
     @Override
     public Tuple7<LocalDateTime, LocalDateTime, Integer, Integer, Integer, Double, Double> map(TaxiRideTuple rideTuple) throws Exception {
         return new Tuple7<LocalDateTime, LocalDateTime, Integer, Integer, Integer, Double, Double>(rideTuple.f1, rideTuple.f2, rideTuple.f3, rideTuple.f5, rideTuple.f9, rideTuple.f13, rideTuple.f16);
     }
-
 }
 
